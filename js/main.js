30e8ca8af896e841ffe0bbd1681fd45997afee26
RaiN <szabogya81@gmail.com>
2021-01-17 12:54:06 +0100

    Header, footer, fonts, colors

--- js/main.js
+++ js/main.js
@@ -1,39 +1,24 @@
 /* eslint-disable no-undef */
 
-'use trict';
+// $(() => {
+//   $('[data-toggle="tooltip"]').tooltip();
+// });
+const mainLinks = document.querySelectorAll('.nav--main .nav__link');
+const mainBrand = document.querySelector('.nav--main .nav__brand');
 
-$(() => {
-  $('[data-toggle="tooltip"]').tooltip();
-});
-
-$(window).scroll(() => {
-  const scroll = $(window).scrollTop();
-
-  if (scroll >= 1) {
-    $('.navbar').removeClass('navbar-dark');
-    $('.sticky-top').addClass('scroll');
-    $('.navbar').addClass('scroll');
+window.onscroll = () => {
+  if (window.scrollY >= 1) {
+    document.querySelector('.nav.nav--main').classList.add('scroll');
   } else {
-    $('.navbar').removeClass('scroll');
-    $('.sticky-top').removeClass('scroll');
-    $('.navbar').addClass('navbar-dark');
+    document.querySelector('.nav.nav--main').classList.remove('scroll');
   }
-});
+};
 
-$(document).ready(() => {
-  // eslint-disable-next-line func-names
-  $('div.navbar-nav > a').click(function () {
-    $('nav.navbar > a.navbar-brand').removeClass('active');
-    $('div.navbar-nav > a').removeClass('active');
-    $(this).addClass('active');
-  });
-});
+function setActiveLink(e) {
+  mainLinks.forEach((link) => link.classList.remove('active'));
+  mainBrand.classList.remove('active');
+  e.currentTarget.classList.add('active');
+}
 
-$(document).ready(() => {
-  // eslint-disable-next-line func-names
-  $('nav.navbar > a.navbar-brand').click(function () {
-    $('div.navbar-nav > a').removeClass('active');
-    $(this).addClass('active');
-    $('.navbar-collapse').collapse('hide');
-  });
-});
+mainLinks.forEach((link) => link.addEventListener('click', setActiveLink));
+mainBrand.addEventListener('click', setActiveLink);
--- js/modal.js
+++ js/modal.js
@@ -1,66 +1,19 @@
-const btn = document.querySelector('.showModal');
-const overlayDiv = document.createElement('div');
-overlayDiv.className = 'overlay';
+const modalLinks = document.querySelectorAll('.footer-link');
 
-const modalDiv = document.querySelector('.modal');
-const modalClose = document.querySelector('.modal__closeButton');
-const modalNo = document.querySelector('.footer__noButton');
-
-btn.addEventListener('click', showModal);
-modalClose.addEventListener('click', closeModal);
-modalNo.addEventListener('click', closeModal);
-window.addEventListener('click', outsideClick);
-
-function closeModal() {
-  fadeOut(modalDiv);
-  setTimeout(() => {
-    modalDiv.classList.remove('modal--active');
-    document.body.removeChild(overlayDiv);
-  }, 300);
+function hideModal(e) {
+  e.currentTarget.parentElement.parentElement.style.display = 'none';
 }
 
-function showModal() {
-  document.body.appendChild(overlayDiv);
-  fadeIn(modalDiv);
-  modal.classList.add('modal--active');
-  modalClose.focus();
+function showModal(e) {
+  e.preventDefault();
+
+  const targetModal = document.querySelector(e.currentTarget.href
+    .substring(e.currentTarget.href.toString().lastIndexOf('#')));
+  targetModal.style.display = 'block';
+  targetModal.querySelectorAll('.close')
+    .forEach((closeButton) => closeButton.addEventListener('click', hideModal));
 }
 
-function fadeIn(element) {
-  let opacity = 0;
-  modalClose.focus();
-  const intervalId = setInterval(() => {
-    if (!element.style.opacity) {
-      element.style.opacity = 1;
-    }
-    if (opacity <= 1) {
-      opacity += 0.1;
-      element.style.opacity = opacity;
-    } else {
-      clearInterval(intervalId);
-    }
-  }, 30);
-
-  modal.classList.add('modal--active');
-  modal.focus();
-  modalClose.focus();
-}
-
-function fadeOut(element) {
-  const intervalID = setInterval(() => {
-    if (!element.style.opacity) {
-      element.style.opacity = 1;
-    }
-    if (element.style.opacity > 0) {
-      element.style.opacity -= 0.1;
-    } else {
-      clearInterval(intervalID);
-    }
-  }, 30);
-}
-
-function outsideClick(e) {
-  if (e.target === overlayDiv) {
-    closeModal();
-  }
-}
+modalLinks.forEach((link) => {
+  link.addEventListener('click', showModal);
+});
